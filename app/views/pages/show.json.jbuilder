ea578cbdf394cbaf6c103975667fe375ea94eb39
horus <smonsalve@gmail.com>
2013-10-21 12:39:02 -0500

    fixin the repo

---
+++ app/views/pages/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @page, :name, :permalink, :content, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Personal::Application.config.secret_key_base = '7b195bc1be62f7bba6b8667672559b91bbf9e81ddf7db332dca092d04fcf42791232733825cfb2f063701454100483a1d3da97d4112842e6e7bdc3db3fd14097'
