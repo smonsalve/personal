b0e34cf0e5e3acc2c2bfc03b8db0c2d035f4e13c
horus <smonsalve@gmail.com>
2013-10-23 15:53:34 -0500

    task to seed the db bundle exec

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,7 +30,7 @@ namespace :deploy do
 
   desc "reload the database with seed data"
   task :seed do
-    run "cd #{current_path}; rake db:seed RAILS_ENV=#{rails_env}"
+    run "cd #{current_path}; bundle exec rake db:seed RAILS_ENV=#{rails_env}"
   end
 
   task :setup_config, roles: :app do
