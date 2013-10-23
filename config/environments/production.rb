982e660e3b9ce434e3895f16c9f3db4b2b196568
horus <smonsalve@gmail.com>
2013-10-23 15:26:40 -0500

    assets fucking assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Personal::Application.configure do
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
   config.serve_static_assets = true
+  config.assets.precompile += %w(readable.js readable.css)
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
